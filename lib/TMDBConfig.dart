e9b0c56f14ca135b20a1ca15b7b494025bdac352
MD092-Hemil <hemil.kumbhani@multidots.com>
2019-03-14 10:48:12 +0530

    -Checking on Animation

--- lib/TMDBConfig.dart
+++ lib/TMDBConfig.dart
@@ -1,4 +1,4 @@
 class TMDBConfig {
   //TODO change name to TMDBConfig from SampleTMDBConfig
-  static final String apiKey = '4f820c5822018f30c6ac562809e4eb65';
+  static final String apiKey = '';
 }
